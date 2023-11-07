de13f85360350f0bb028a778fa1f61b9c9843dbe
alulestad <vladimirlestad@gmail.com>
2023-11-07 18:58:14 -0500

    mi primer commit

---
+++ src/main/java/com/uce/edu/Pa2U1P5JjRgApplication.java
@@ -0,0 +1,18 @@
+package com.uce.edu;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class Pa2U1P5JjRgApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pa2U1P5JjRgApplication.class, args);
+	}
+
+	public void run(String... args) throws Exception{
+		System.out.println("Hola Mundo ");
+		System.out.println("Soy Jose Jimenez y Romina Guevara");
+	}
+	
+}
